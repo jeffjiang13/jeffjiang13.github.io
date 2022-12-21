09d995e35a8b655c4a649c28f3da4e32ba271d52
Jeff Jiang <jeff.jiang13@gmail.com>
2022-12-21 16:49:09 -0500

    worked on email

--- mail.php
+++ mail.php
@@ -1,6 +1,6 @@
 <?php
 
-	$emailTo = "emailto:jeff.jiang13@gmail.com", "test@test.com"; // Enter your email for feedbacks here
+	$emailTo = "mailto:jeff.jiang13@gmail.com", "test@test.com"; // Enter your email for feedbacks here
 
 	$headers = "MIME-Version: 1.0\r\n";
 	$headers .= "Content-type: text/html; charset=utf-8\r\n";
